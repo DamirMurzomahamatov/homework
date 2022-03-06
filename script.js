3ce17a61ff46de7f8b63c310b592b786e52d89e5
DamirMurzomahamatov <you@example.com>
2022-03-06 18:09:12 +0500

    homework

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let name =prompt('alex')
-switch (name) {
-    case 'alex':
-       console.log('проходишь');
-        break;
-    case 'алекс':
-  console.log('проходишь');
-  break;
-    default:
-        console.log('soso');
-        break;
+// switch (name) {
+//     case 'alex':
+//        console.log('проходишь');
+//         break;
+//     case 'алекс':
+//   console.log('проходишь');
+//   break;
+//     default:
+//         console.log('soso');
+//         break;
+// }
+if (name.toLowerCase()==='alex') {
+alert('проходишь');
+} else if(name.toLowerCase()==='алекс'){
+    alert('проходишь');
 }
- 
+else{
+alert(`${name} идёт домой`);
+} 
