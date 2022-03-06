55e1b643caf528c0ac65c573ef0efbbae37760d5
DamirMurzomahamatov <you@example.com>
2022-03-06 17:13:09 +0500

    mein

---
+++ script.js
@@ -0,0 +1,13 @@
+let name =prompt('alex')
+switch (name) {
+    case 'alex':
+       console.log('проходишь');
+        break;
+    case 'алекс':
+  console.log('проходишь');
+  break;
+    default:
+        console.log('soso');
+        break;
+}
+ 
